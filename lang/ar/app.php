c01c82a3c1ba481ee40a547b8aa37e9f0740a212
MedjadjiAbdelkadir <medjadjiabdelkadir22@gmail.com>
2024-03-11 16:13:57 +0100

    add all file of language ar

---
+++ lang/ar/app.php
@@ -0,0 +1,64 @@
+<?php
+
+return [
+    'actions'   => 'العمليات',
+    'close'     => 'إغلاق',
+
+    'create'    => 'إظافة',
+    'edit'      => 'تعديل',
+    'update'    => 'تحديث',
+    'delete'    => 'حذف',
+    'save'      => 'حفظ البيانات',
+    'cancel'    => 'إلغاء',
+    'login'    => 'تسجيل الدخول',
+    'register' => 'إنشاء حساب',
+    'logout'   => 'تسجيل الخروج',
+    'profile'  => 'معلومات الحساب',
+
+    'select_gender' => 'حدد الجنس',
+    'female' => 'أنثى',
+    'male' => 'ذكر',
+
+    'review'  => 'التقييم',
+    
+
+    'label' =>[
+        'name'   => 'الإسم',
+        'phone'  => 'رقم الهاتف',
+        'email'  => 'البريد الإلكتروني',
+        'password'          => 'كلمة السر',
+        'confirme_password' => 'تأكيد كلمة السر',
+        'firstname_fr' => 'الإسم بالفرنسية',
+        'lastname_fr'  => 'اللقب بالفرنسية',
+        'firstname_ar' => 'الإسم بالعربية',
+        'lastname_ar'  => 'اللقب بالعربية',
+        'birthday'     => 'تاريخ الميلاد',
+        'gender'       => 'الجنس',
+        'state_of_birth' => 'ولاية الميلاد',
+        'place_of_birth' => 'مكان الميلاد',
+        'state_place_of_birth' => 'ولاية ومكان الميلاد',
+        'group' => 'الفوج',
+        'registration_number' => 'رقم التسجيل',
+        'address' => 'العنوان',
+    ],
+    'placeholder' => [
+        'name' => 'أدخل الإسم',
+        'phone'    => 'أدخل رقم الهاتف',
+        'email'    => 'أدخل البريد الإلكتروني',
+        'password' => 'كلمة السر',
+        'firstname_ar' => 'أدخل الإسم بالعربية',
+        'lastname_ar'  => 'أدخل اللقب بالعربية',
+
+        'firstname_fr' => 'Entrez le prenom en français',
+        'lastname_fr'  => 'Entrez le nom de famille en français',
+
+        'birthday' => 'تاريخ الميلاد',
+        'gender'   => 'الجنس',
+        'state_of_birth' => 'أدخل ولاية الميلاد',
+        'place_of_birth' => 'أدخل مكان الميلاد',
+        'state_place_of_birth'=> 'أدخل ولاية ومكان الميلاد',
+        'registration_number' => 'أدخل رقم التسجيل',
+        'group' => 'أدخل الفوج',
+        'address' => 'أدخل العنوان',
+    ],
+];
---
+++ lang/ar/student.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+    'title' => 'الطلاب',
+    'dashboard' => 'لوحــة التحكـــــم',
+    'students' => 'الطلاب',
+    'title-dashboard' => 'الصفحة الرئيسية',
+    'profile' => 'معلومات الحساب',
+
+    'name' => 'الإسم',
+    'birthday' => 'تاريخ الميلاد',
+    'gender' => 'الجنس',
+    'female' => 'أنثى',
+    'male' => 'ذكر',
+    'state_place_of_birth' => 'ولاية ومكان الميلاد',
+
+    'registration_number' => 'رقم التسجيل',
+    'group' => 'الفوج',
+
+    'phone' => 'رقم الهاتف',
+    'email' => 'البريد الإلكتروني',
+    
+    
+    'create'=> 'إظافة طالب',
+    'edit' => 'تعديل بيانات الطالب',
+    'delete' => 'حذف الطالب',
+    'delete_confirmation' => 'هل أنت متأكد من عملية حذف هذ الطالب ؟',
+
+    'label' =>[
+        'name' => 'الإسم',
+        'group' => 'الفوج',
+        'registration_number' => 'رقم التسجيل',
+    ],
+    'placeholder' =>[
+        'name' => 'البحث بالإسم',
+        'group' => 'البحث بالفوج',
+        'registration_number' => 'البحث برقم التسجيل'
+    ],
+];
