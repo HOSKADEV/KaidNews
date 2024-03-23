d793f158d920df77a203a6ad90aac1aa70958227
MedjadjiAbdelkadir <medjadjiabdelkadir22@gmail.com>
2024-03-23 17:41:52 +0100

    Update StoreStudentRequest and UpdateStudentRequest when unique fields

--- app/Http/Requests/Student/UpdateStudentRequest.php
+++ app/Http/Requests/Student/UpdateStudentRequest.php
@@ -43,8 +43,8 @@ class UpdateStudentRequest extends FormRequest
 
             'phone' => 'numeric|unique:students,phone,'.$this->id,
             'email' => 'email|unique:students,email,'.$this->id,
-            'password' => 'min:8|max:255',
-            'password_confirmation' => 'same:password|min:8|max:255'
+            'password' => 'sometimes|min:8:max:255',
+            'password_confirmation' => 'sometimes|same:password|min:8|max:255'
         ];
     }
     public function attributes()
