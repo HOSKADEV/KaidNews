655a84de6992cbc01e6b643813bfd84e21321610
hoskamohamed <addamohamed@hoskadev.com>
2024-05-12 09:24:32 +0100

    12 05 24

--- resources/views/dashboard/printer/attendence.blade.php
+++ resources/views/dashboard/printer/attendence.blade.php
@@ -140,7 +140,7 @@
                             @for ($day = 1; $day <= $days; $day++)
                                 <td class="text-center" style="width: 30px">
                                     @if (count($student->attendences->where('week', $i)->where('month', $month)->where('year', $year)->where('day', $day)) > 0)
-                                        X
+                                        X - {{ count($student->attendences->where('week', $i)->where('month', $month)->where('year', $year)->where('day', $day)) }}
                                     @endif
                                 </td>
                             @endfor
